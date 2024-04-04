30be4c84241852cc83e7f370c5587357ad54ba2a
unknown <cho89@live.co.kr>
2024-04-04 11:31:35 +0900

    npm delete and video

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createManager } from '@vue-youtube/core';
 import App from './App.vue'
-import VueRecord from '@codekraft-studio/vue-record'
 
-createApp(App).use(createManager()).use(VueRecord).mount('#app')
+createApp(App).use(createManager()).mount('#app')
